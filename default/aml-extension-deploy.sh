d87c6b278fd93d960f8b5d5e5c2f0ed252079c09
Diego Casati <diego.casati@microsoft.com>
2022-09-23 17:24:45 +0000

    shellcheck and shfmt

--- default/aml-extension-deploy.sh
+++ default/aml-extension-deploy.sh
@@ -1,70 +1,83 @@
-#! /bin/bash
+load_env() {
+  export TF_OUTPUTS=$(terraform output -json | jq -r)
 
-export TF_OUTPUTS=$(terraform output -json | jq -r)
-
-export AKS_CLUSTER_NAME=$(echo $TF_OUTPUTS | jq -r .aks_cluster_name.value)
-export RGNAME=$(echo $TF_OUTPUTS | jq -r .resource_group_name.value)
-export AML_WORKSPACE_NAME=$(echo $TF_OUTPUTS | jq -r .aml_workspace_name.value)
-export AML_K8S_NAMESPACE="azure-ml"
-export SUBSCRIPTION_ID=$(echo $TF_OUTPUTS | jq -r .subscription_id.value)
+  export AKS_CLUSTER_NAME=$(echo "$TF_OUTPUTS" | jq -r .aks_cluster_name.value)
+  export RGNAME=$(echo $TF_OUTPUTS | jq -r .resource_group_name.value)
+  export AML_WORKSPACE_NAME=$(echo "$TF_OUTPUTS" | jq -r .aml_workspace_name.value)
+  export AML_K8S_NAMESPACE="azure-ml"
+  export SUBSCRIPTION_ID=$(echo "$TF_OUTPUTS" | jq -r .subscription_id.value)
+}
 
 ## Install AML Extension for K8s Cluster (AKS)
 ## https://learn.microsoft.com/en-us/azure/machine-learning/how-to-deploy-kubernetes-extension?tabs=deploy-extension-with-cli#azureml-extension-deployment---cli-examples-and-azure-portal
+azureml_providers() {
+  az provider register --namespace Microsoft.KubernetesConfiguration
+  az extension add --name k8s-extension
+  az extension add --name ml
+}
 
-az provider register --namespace Microsoft.KubernetesConfiguration
-az extension add --name k8s-extension
-az extension add --name ml
-
-# For demo setup:
-
-az k8s-extension create \
+azureml_setup() {
+  # For demo setup:
+  az k8s-extension create \
     --name aml-compute \
     --extension-type Microsoft.AzureML.Kubernetes \
     --config \
-        enableTraining=True \
-        enableInference=True \
-        inferenceRouterServiceType=LoadBalancer \
-        allowInsecureConnections=True \
-        inferenceLoadBalancerHA=False \
+    enableTraining=True \
+    enableInference=True \
+    inferenceRouterServiceType=LoadBalancer \
+    allowInsecureConnections=True \
+    inferenceLoadBalancerHA=False \
     --cluster-type managedClusters \
-    --cluster-name $AKS_CLUSTER_NAME \
-    --resource-group $RGNAME \
+    --cluster-name "$AKS_CLUSTER_NAME" \
+    --resource-group "$RGNAME" \
     --scope cluster \
     --debug
 
-## For prod training and inference: 
-#
-# az k8s-extension create \
-#     --name <extension-name> \
-#     --extension-type Microsoft.AzureML.Kubernetes \
-#     --config \
-#         enableTraining=True \
-#         enableInference=True \
-#         inferenceRouterServiceType=LoadBalancer \
-#         sslCname=<ssl cname> \
-#     --config-protected \
-#         sslCertPemFile=<file-path-to-cert-PEM> \
-#         sslKeyPemFile=<file-path-to-cert-KEY> \
-#     --cluster-type managedClusters \
-#     --cluster-name $AKS_CLUSTER_NAME \
-#     --resource-group $RGNAME \
-#     --scope cluster
+  ## For prod training and inference:
+  #
+  # az k8s-extension create \
+  #     --name <extension-name> \
+  #     --extension-type Microsoft.AzureML.Kubernetes \
+  #     --config \
+  #         enableTraining=True \
+  #         enableInference=True \
+  #         inferenceRouterServiceType=LoadBalancer \
+  #         sslCname=<ssl cname> \
+  #     --config-protected \
+  #         sslCertPemFile=<file-path-to-cert-PEM> \
+  #         sslKeyPemFile=<file-path-to-cert-KEY> \
+  #     --cluster-type managedClusters \
+  #     --cluster-name $AKS_CLUSTER_NAME \
+  #     --resource-group $RGNAME \
+  #     --scope cluster
+}
 
 ## Attach AKS to an AML workspace
 ## https://learn.microsoft.com/en-us/azure/machine-learning/how-to-attach-kubernetes-to-workspace?tabs=cli
-
-az ml compute attach \
-    --resource-group $RGNAME \
-    --workspace-name $AML_WORKSPACE_NAME \
+azureml_attach() {
+  az ml compute attach \
+    --resource-group "$RGNAME" \
+    --workspace-name "$AML_WORKSPACE_NAME" \
     --type Kubernetes \
     --name k8s-compute \
     --resource-id "/subscriptions/$SUBSCRIPTION_ID/resourceGroups/$RGNAME/providers/Microsoft.ContainerService/managedclusters/$AKS_CLUSTER_NAME" \
     --identity-type SystemAssigned \
     --namespace $AML_K8S_NAMESPACE \
     --debug
+}
 
-# az k8s-extension show \
-#     --name azureml \
-#     --cluster-name <clusterName> \
-#     --resource-group <resourceGroupName> \
-#     --cluster-type managedClusters
+azureml_show() {
+  az k8s-extension show \
+    --name azureml \
+    --cluster-name "$AKS_CLUSTER_NAME" \
+    --resource-group "$RGNAME" \
+    --cluster-type managedClusters
+}
+
+demo_entrypoint() {
+  load_env
+  azureml_providers
+  azureml_setup
+  azureml_attach
+  azureml_show
+}
--- default/run.sh
+++ default/run.sh
@@ -1,11 +1,15 @@
 #!/usr/bin/env bash
+# shfmt -i 2 -ci -w
 set -e
+
 # Requirements:
 # - Azure CLI
 # - helm
 # - jq
 # - kubectl
 # - terraform
+
+source aml-extension-deploy.sh
 
 __usage="
 Available Commands:
@@ -18,92 +22,97 @@ Available Commands:
 "
 
 usage() {
-    echo "usage: ${0##*/} [options]"
-    echo "${__usage/[[:space:]]/}"
-    exit 1
+  echo "usage: ${0##*/} [options]"
+  echo "${__usage/[[:space:]]/}"
+  exit 1
 }
 
 timestamp() {
-    date +"%r"
+  date +"%r"
 }
 
 checkDependencies() {
-    # check if the dependencies are installed
-    _NEEDED="az jq kubectl terraform"
+  # check if the dependencies are installed
+  _NEEDED="az jq kubectl terraform"
 
-    echo -e "Checking dependencies ...\n"
-    for i in seq ${_NEEDED}
-        do
-           if hash "$i" 2>/dev/null; then
-               # do nothing
-               :
-           else
-               echo -e "\t $_ not installed".
-               _DEP_FLAG=true
-           fi
-        done
-
-    if [[ "${_DEP_FLAG}" == "true" ]]; then
-            echo -e "\nDependencies missing. Please fix that before proceeding"
-            exit 1
+  echo -e "Checking dependencies ...\n"
+  for i in seq ${_NEEDED}; do
+    if hash "$i" 2>/dev/null; then
+      # do nothing
+      :
+    else
+      echo -e "\t $_ not installed".
+      _DEP_FLAG=true
     fi
+  done
+
+  if [[ "${_DEP_FLAG}" == "true" ]]; then
+    echo -e "\nDependencies missing. Please fix that before proceeding"
+    exit 1
+  fi
 }
 
-terraformDance () {
-# Assumes you're already logged into Azure
-    terraform init
-    terraform plan -out tfplan
-    terraform apply -auto-approve tfplan
+do_demo_bootstrap() {
+  demo_entrypoint
+}
+
+terraformDance() {
+  # Assumes you're already logged into Azure
+  terraform init
+  terraform plan -out tfplan
+  terraform apply -auto-approve tfplan
 }
 
 show() {
-   terraform output -json | jq -r 'to_entries[] | [.key, .value.value]'
+  terraform output -json | jq -r 'to_entries[] | [.key, .value.value]'
 }
 
 destroy() {
-    # remove all of the infrastructured
-    terraform destroy -auto-approve
-    rm -rf \
-        terraform.tfstate \
-        terraform.tfstate.backup \
-        tfplan \
-        .terraform \
-        .terraform.lock.hcl
+  # remove all of the infrastructured
+  terraform destroy -auto-approve
+  rm -rf \
+    terraform.tfstate \
+    terraform.tfstate.backup \
+    tfplan \
+    .terraform \
+    .terraform.lock.hcl
 }
 
 run() {
-    terraformDance
+  terraformDance
+  demo_entrypoint
 }
 
 exec_case() {
-    local _opt=$1
+  local _opt=$1
 
-    case ${_opt} in
-    install)    checkDependencies && run;;
-    destroy)    checkDependencies && destroy;;
-    show)       show;;
-    *)          usage;;
-    esac
-    unset _opt
+  case ${_opt} in
+    install) checkDependencies && run ;;
+    destroy) checkDependencies && destroy ;;
+    show) show ;;
+    *) usage ;;
+  esac
+  unset _opt
 }
 
-while getopts "o:x:" opt; do
-    case $opt in
-    x)  exec_flag=true
-        EXEC_OPT="${OPTARG}"
-        ;;
-    *)  usage;;
-    esac
+while getopts "x:" opt; do
+  case $opt in
+    x)
+      exec_flag=true
+      EXEC_OPT="${OPTARG}"
+      ;;
+    *) usage ;;
+  esac
 done
-shift $(( OPTIND - 1 ))
+shift $((OPTIND - 1))
 
 if [ $OPTIND = 1 ]; then
-    usage
-    exit 0
+  usage
+  exit 0
 fi
 
 if [[ "${exec_flag}" == "true" ]]; then
-    exec_case "${EXEC_OPT}"
+  exec_case "${EXEC_OPT}"
 fi
 
 exit 0
