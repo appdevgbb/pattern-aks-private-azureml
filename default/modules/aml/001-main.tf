77852e045671b60062b1d76eefd0b15984579e04
Ray Kao <raykao@gmail.com>
2022-09-25 20:17:20 -0400

    ensure all resources are cleaned up

--- default/modules/aml/001-main.tf
+++ default/modules/aml/001-main.tf
@@ -1,5 +1,9 @@
 provider "azurerm" {
-  features {}
+  features {
+    resource_group {
+      prevent_deletion_if_contains_resources = false
+    }
+  }
 }
 
 data "azurerm_client_config" "current" {}
