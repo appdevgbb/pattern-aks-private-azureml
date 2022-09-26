8049b15020f6f6a0a4e00ffa29ecf778e940438e
Ray Kao <raykao@gmail.com>
2022-09-25 23:17:55 -0400

    fix: monitor diag was causing unncessary updates

--- default/modules/firewall/azurerm-monitor-diagnostic-setting.tf
+++ default/modules/firewall/azurerm-monitor-diagnostic-setting.tf
@@ -3,24 +3,148 @@ resource "azurerm_monitor_diagnostic_setting" "example" {
   name               = "${azurerm_firewall.default.name}-diag-settings"
   target_resource_id = azurerm_firewall.default.id
   log_analytics_workspace_id = azurerm_log_analytics_workspace.azfw.id
-
-  log {
-    category = "AzureFirewallApplicationRule"
-    enabled  = true
-  }
-
-  log {
-    category = "AzureFirewallNetworkRule"
-    enabled  = true
-  }
-
-  log {
-    category = "AzureFirewallDnsProxy"
-    enabled  = true
-  }
+  log_analytics_destination_type = "AzureDiagnostics"
 
   metric {
     category = "AllMetrics"
     enabled = true
   }
+
+  log {
+    category = "AzureFirewallNetworkRule"
+    enabled = true
+    retention_policy {
+      days = 0
+      enabled = false
+    }
+  }
+
+  log {
+    category = "AzureFirewallApplicationRule"
+    enabled  = true
+    retention_policy {
+      days = 0
+      enabled = false
+    }
+  }
+
+  log {
+    category = "AzureFirewallDnsProxy"
+    enabled  = true
+    retention_policy {
+      days = 0
+      enabled = false
+    }
+  }
+
+  log {
+    category = "AZFWThreatIntel"
+    enabled = false
+    retention_policy {
+      days = 0
+      enabled = false
+    }
+  }
+
+  log {
+    category = "AZFWNetworkRuleAggregation"
+    enabled = false
+    retention_policy {
+      days = 0
+      enabled = false
+    }
+  }
+
+  log {
+    category = "AZFWNetworkRule"
+    enabled = false
+    retention_policy {
+      days = 0
+      enabled = false
+    }
+  }
+
+  log {
+    category = "AZFWNatRuleAggregation"
+    enabled = false
+    retention_policy {
+      days = 0
+      enabled = false
+    }
+  }
+
+  log {
+    category = "AZNatRule"
+    enabled = false
+    retention_policy {
+      days = 0
+      enabled = false
+    }
+  }
+
+  log {
+    category = "AZFWIdpsSignature"
+    enabled = false
+    retention_policy {
+      days = 0
+      enabled = false
+    }
+  }
+
+  log {
+    category = "AZFWApplicationRule"
+    enabled  = false
+
+    retention_policy {
+      days    = 0
+      enabled = false
+    }
+  }
+
+  log {
+    category = "AZFWApplicationRuleAggregation"
+    enabled  = false
+
+    retention_policy {
+      days    = 0
+      enabled = false
+    }
+  }
+
+  log {
+    category = "AZFWDnsQuery"
+    enabled  = false
+
+    retention_policy {
+      days    = 0
+      enabled = false
+    }
+  }
+
+  log {
+    category = "AZFWFatFlow"
+    enabled  = false
+
+    retention_policy {
+      days    = 0
+      enabled = false
+    }
+  }
+
+  log {
+    category = "AZFWFqdnResolveFailure"
+    enabled  = false
+
+    retention_policy {
+      days    = 0
+      enabled = false
+    }
+  }
+
+  lifecycle {
+    ignore_changes = [
+      log
+    ]
+  }
+
 }
